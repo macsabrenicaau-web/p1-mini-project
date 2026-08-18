09469c4998e4091d36065a1e7709bc3c37c2530b
Macvhil Abrenica <macs.abrenica.au@phinmaed.com>
2026-08-18 13:02:10 +0800

    Delete main directory

--- main/Main.py
+++
@@ -1,9 +0,0 @@
-print("Hello, world")
-
-name = input("Enter your name:")
-print(name)
-age = int(input("Enter your age:"))
-print("Your age is:", age)
-next_year_age = age + 1
-print("Next year, you will be:", next_year_age)
-
